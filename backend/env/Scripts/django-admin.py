482b7281a09e989f80ff0b558cf187f15138f151
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2020-05-15 00:24:07 +0500

    first commit

---
+++ backend/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\syedf\onedri~1\djreact\backend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ backend/src/articles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField()
+    message=models.TextField()
+
+    def __str__(self):
+        return self.name
+    
